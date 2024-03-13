e5624ea9cf6c7fe8e45d0c9a49c5c5597e0e1def
remi-vidal-michel <remi.vidal-michel@laplateforme.io>
2024-03-13 16:18:16 +0100

    Add code to write a binary file with a header structure

--- src/main.c
+++ src/main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World\n");
+    struct {
+        char magic[4];
+        unsigned int size;
+        char name[16];
+    } header = {
+        .magic = {'C', 'O', 'D', 'E'}, 
+        .size = 0, 
+        .name = {'C','O','M','P','I','L','E','R'}
+    };
+
+    ((unsigned char * )(&header.size))[3] = 0x0c;
+
+    // unsigned char data[] = {0x01, 0x00, 0x00, 0x00, 0x03, 0x01, 0x00, 0x00, 0x00, 0x04, 0x04, 0x12};
+
+    FILE *fichier = fopen("code.bin", "wb");
+    if (fichier != NULL) {
+        fwrite(&header, sizeof(header), 1, fichier);
+        // fwrite(data, sizeof(data), 1, fichier);
+        fclose(fichier);
+    } else {
+        printf("Erreur lors de l'ouverture du fichier.\n");
+    }
     return 0;
 }
-
